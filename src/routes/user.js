19aae6b9dbfbedc00b4357a7980bf436bf5fe9e4
Bhargav Jitendra Kasundra <bhargav111.kasundra@gmail.com>
2025-11-10 23:52:29 +0530

    cursor based feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,36 +6,36 @@ const User = require("../models/user");
 
 
 // get all pending request from the user
-userRouter.get("/user/requests", userAuth, async (req,res) => {
-    try{
+userRouter.get("/user/requests", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
-        
+
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
-            status:"interested"
-        }).populate("fromUserId",["firstName","lastName","age","gender","about","skills","photoUrl"]);
- 
+            status: "interested"
+        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "about", "skills", "photoUrl"]);
+
         res.json({
             meessage: "connection requests Fetch successfully !!",
             data: connectionRequests
         });
 
-    } catch(err){
-        res.status(400).send("Error: "+err.messaege);
+    } catch (err) {
+        res.status(400).send("Error: " + err.messaege);
     }
 });
 
 // get all connections
-userRouter.get("/user/connections", userAuth, async (req,res) => {
-    try{
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
         const Connections = await ConnectionRequest.find({
             $or: [
-                {fromUserId: loggedInUser._id, status: "accepted"},
-                {toUserId: loggedInUser._id, status: "accepted"}
+                { fromUserId: loggedInUser._id, status: "accepted" },
+                { toUserId: loggedInUser._id, status: "accepted" }
             ]
-        }).populate("fromUserId",["firstName","lastName","age","gender","about","skills","photoUrl"])
-        .populate("toUserId",["firstName","lastName","age","gender","about","skills","photoUrl"]);
+        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "about", "skills", "photoUrl"])
+            .populate("toUserId", ["firstName", "lastName", "age", "gender", "about", "skills", "photoUrl"]);
 
         const formatedConnections = Connections.map(conn => {
             const otherUser = conn.fromUserId._id.equals(loggedInUser._id)
@@ -49,52 +49,61 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
             meessage: "connections Fetched successfully !!",
             data: formatedConnections
         });
-    }catch(err){
-        res.status(400).send("Error: "+err.meessage);
+    } catch (err) {
+        res.status(400).send("Error: " + err.meessage);
     }
 });
 
 // very important , can be interview question (system design)
-userRouter.get("/user/feed", userAuth, async(req, res) => {
-    try{
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
-        const page = parseInt(req.query.page) || 1;
         let limit = parseInt(req.query.limit) || 10;
         // sanitize data
-        limit = limit>50? 50: limit;
-        const skip = (page-1)*limit;
+        limit = limit > 50 ? 50 : limit;
+        const lastUserId = req.query.lastUserId;
 
         const connectionRequests = await ConnectionRequest.find({
             $or: [
-                {fromUserId: loggedInUser._id},
-                {toUserId: loggedInUser._id},
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id },
             ]
-        }).select(["fromUserId","toUserId"]);
-        console.log("connectionRequests:", connectionRequests);
+        }).select(["fromUserId", "toUserId"]);
 
         // using set datastructure to store unique id's
         const hideUsersFromFeed = new Set();
+        hideUsersFromFeed.add(loggedInUser._id.toString());
 
         connectionRequests.forEach((req) => {
             hideUsersFromFeed.add(req.fromUserId?.toString());
             hideUsersFromFeed.add(req.toUserId?.toString());
         });
 
-        const userFeed = await User.find({
-            $and: [
-                {_id: {$nin: Array.from(hideUsersFromFeed)}},
-                {_id: {$ne: loggedInUser._id}},
-            ],
-        }).select(["firstName", "lastName", "age", "gender", "about", "skills", "photoUrl", "_id"])
-        .skip(skip)
-        .limit(limit);
+        let query = { _id: { $nin: Array.from(hideUsersFromFeed) } };
 
-        res.status(200).json({Message:"feed fetched successfully!!",
-            Data: userFeed
+        if (lastUserId) {
+            // to fetch records after lastUserId for pagination
+            query._id.$gt = lastUserId;
+            // or query = {...query._id, $gt: lastUserId}
+        }
+
+        const userFeed = await User.find(query)
+            .select(["firstName", "lastName", "age", "gender", "about", "skills", "photoUrl", "_id"])
+            .limit(limit);
+
+        const nextCursor = userFeed.length > 0 ? userFeed[userFeed.length - 1]._id : null;
+
+
+        res.status(200).json({
+            success: true,
+            message: "Feed fetched successfully!",
+            count: userFeed.length,
+            nextCursor,
+            data: userFeed,
         });
-        
-    }catch(err){
-        res.status(400).send("Error: "+err);
+
+    } catch (err) {
+        res.status(400).send("Error: " + err);
     }
 })
 
